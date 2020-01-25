185d5fa8a5c83cf2c60eb269743e7bc597724702
suspect22 <{ID}+{username}@users.noreply.github.com>
2020-01-25 19:14:17 +0100

    raised version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference  = 'Stop';
-$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+#$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://github.com/gpodder/gpodder/releases/download/3.10.11/windows-gpodder-3.10.11-installer.exe'
+$url                    = 'https://github.com/gpodder/gpodder/releases/download/3.10.12/windows-gpodder-3.10.12-installer.exe'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
@@ -10,10 +10,10 @@ $packageArgs = @{
 
   softwareName          = 'gPodder*'
 
-  checksum              = 'D6E4E7A9D06CB419147A1AD49F8199B99D75EC802DC9FE6DA24C1061384D0B7D'
+  checksum              = 'b5271f7cb3080314c55cb6f5e0238e2a971a18a72895e80d5650fec455a2bbd0'
   checksumType          = 'sha256'
   silentArgs            = '/S'
   validExitCodes        = @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage @packageArgs 
