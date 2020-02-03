be532421bf820900be8ce9fc6bfa0e237eb7798e
suspect22 <{ID}+{username}@users.noreply.github.com>
2020-02-03 10:12:12 +0100

    Version Update + automatic Upgrade

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,35 @@
 ﻿$ErrorActionPreference  = 'Stop';
 #$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://github.com/gpodder/gpodder/releases/download/3.10.12/windows-gpodder-3.10.12-installer.exe'
+$url                    = 'https://github.com/gpodder/gpodder/releases/download/3.10.13/windows-gpodder-3.10.13-installer.exe'
+$softwareName           = 'gPodder'
+$silentArgs             = "/S"
+$validExitCodes         = @(0)
+$fileType               = 'exe'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
-  fileType              = 'exe'
+  fileType              = $fileType
   url                   = $url
 
-  softwareName          = 'gPodder*'
+  softwareName          = "$softwareName*"
 
-  checksum              = 'b5271f7cb3080314c55cb6f5e0238e2a971a18a72895e80d5650fec455a2bbd0'
+  checksum              = '9d28ed5a0b57387cc9166d9d653d43b983d4bcf202dd6a6f7dac400b032aa31c'
   checksumType          = 'sha256'
-  silentArgs            = '/S'
-  validExitCodes        = @(0)
+  silentArgs            = $silentArgs
+  validExitCodes        = $validExitCodes
 }
+
+
+# silent uninstallation/upgrading of gpodder is not possible via /S Command
+# Check if gPodder is already installed
+[array]$gpodderRegistryKey = Get-UninstallRegistryKey -SoftwareName "$softwareName*"
+if ($gpodderRegistryKey.count -gt 0){
+
+  $UninstallStringFilePath = $gpodderRegistryKey.UninstallString
+    Uninstall-ChocolateyPackage -PackageName $softwareName -FileType $fileType -SilentArgs $silentArgs -File $UninstallStringFilePath
+}
+
+
 
 Install-ChocolateyPackage @packageArgs 
